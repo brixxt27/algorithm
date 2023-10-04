15dbbf93325e22006f766e76c3ff36cc1df74be6
brixxt27 <brixxt27@gmail.com>
2023-10-05 04:52:33 +0900

    chore: directory 구조 변경

---
+++ day1/Main.java
@@ -0,0 +1,12 @@
+package day1;
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int W = scanner.nextInt();
+        int R = scanner.nextInt();
+        int RM = (int)(W * (1 + (double)R / 30));
+
+        System.out.println(RM);
+    }
+}
